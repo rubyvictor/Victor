4fea10989686249f64caed98f5eb72e0608e304a
Victor Lee <myleevictor@gmail.com>
2018-05-07 17:42:55 +0800

    removed socialLinks except github, add url for github and clean up Footer

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -1,37 +1,11 @@
 import React from "react";
-import {
-  FaFacebook,
-  FaGithub,
-  FaGooglePlus,
-  FaInstagram,
-  FaTwitter
-} from "react-icons/lib/fa";
+import { FaGithub } from "react-icons/lib/fa";
 // https://gorangajic.github.io/react-icons/fa.html
 
 const SocialLinks = () => (
   <ul className="social">
     <li>
-      <a href="#">
-        <FaFacebook />
-      </a>
-    </li>
-    <li>
-      <a href="#">
-        <FaTwitter />
-      </a>
-    </li>
-    <li>
-      <a href="#">
-        <FaGooglePlus />
-      </a>
-    </li>
-    <li>
-      <a href="#">
-        <FaInstagram />
-      </a>
-    </li>
-    <li>
-      <a href="#">
+      <a href="https://github.com/rubyvictor">
         <FaGithub />
       </a>
     </li>
